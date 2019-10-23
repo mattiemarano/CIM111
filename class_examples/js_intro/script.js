06f1f728739e3fe4e4c261144002bc8bfdeed7de
Mattie Marano <mwm89@miami.edu>
2019-10-23 13:33:45 -0400

    introduction to jquery

---
+++ class_examples/js_intro/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    //all your javascript goes here
+
+    //how to add numbers
+    var x = 2+2;
+    // be careful where you put your quotations:
+    var y= 2+"2"; // + puts strings together
+    // defining a variable;
+    var z;
+
+
+    //using a defined variable
+
+    z= "My name is Mattie";
+    // console.log(x);
+    // console.log("Everything loaded!");
+    // console.log("this function ran");
+  });
+
+
+
+
+
+
+
+
+
+
+  // var array1 = [1,2,3,4,5,6]; //arrays are fancy ways to write lists
+  // var array2 = ["This is a string", 'This is also a string'] // a string: between quotation marks; both together are an array
+  // var heading = document.getElementsByTagName("h1");
+  //   console.log(heading);
+  //
+  //   <script src="js/jquery-3.3.1.min.js"></script>
+
+  //jquery
+  //$() whatever is in there will be affected
